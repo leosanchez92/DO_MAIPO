dedc1e3780f6e5bb3a78f367736d29a0d1f3273c
leosanchez92 <nelsonsanchez.al@gmail.com>
2022-06-09 23:09:26 -0400

    Initial commit

---
+++ data/vinas.js
@@ -0,0 +1,13 @@
+var vinas_maipo = {
+"type": "FeatureCollection",
+"name": "vinas",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ID": 1, "TIPO": "VI", "VINNA": "Aquitania", "UTM_X": 356190.012, "UTM_Y": 6293317.356 }, "geometry": { "type": "Point", "coordinates": [ -70.547999993989862, -33.489999874903027 ] } },
+{ "type": "Feature", "properties": { "ID": 2, "TIPO": "VI","VINNA": "Pérez Cruz", "UTM_X": 350018.171, "UTM_Y": 6254291.71 }, "geometry": { "type": "Point", "coordinates": [ -70.620999994186576, -33.840999879309557 ] } },
+{ "type": "Feature", "properties": { "ID": 3, "TIPO": "VI","VINNA": "Almaviva", "UTM_X": 349703.266, "UTM_Y": 6280241.47 }, "geometry": { "type": "Point", "coordinates": [ -70.619999999213107, -33.60699987926521 ] } },
+{ "type": "Feature", "properties": { "ID": 4, "TIPO": "VI","VINNA": "De Martino", "UTM_X": 324570.154, "UTM_Y": 6263951.72 }, "geometry": { "type": "Point", "coordinates": [ -70.894000001349042, -33.74999987094796 ] } },
+{ "type": "Feature", "properties": { "ID": 5, "TIPO": "VI","VINNA": "Hartas de Pirque", "UTM_X": 350773.569, "UTM_Y": 6268731.798 }, "geometry": { "type": "Point", "coordinates": [ -70.610399996020561, -33.71091987621886 ] } },
+{ "type": "Feature", "properties": { "ID": 6, "TIPO": "VI","VINNA": "Tres Palacios", "UTM_X": 305899.872, "UTM_Y": 6256933.534 }, "geometry": { "type": "Point", "coordinates": [ -71.096999998819399, -33.809999860776976 ] } }
+]
+}
